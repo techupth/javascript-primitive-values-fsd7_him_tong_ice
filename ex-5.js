ff34805e70d0811203ba67fe3c8f31138f774083
tong <reangsak1234@hotmail.com>
2024-09-30 14:21:51 +0700

    feat: JS primitive value assignment

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let discount25Percent = 0.25 * totalPrice;
+let priceAfterDiscount = totalPrice - discount25Percent;
+console.log(priceAfterDiscount);
